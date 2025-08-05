256093b87cb9e1c062a453bf5c8dda7a1454a753
yash1031 <yash735131@gmail.com>
2025-08-05 21:03:13 +0530

    Sixth Commit

--- lambda_function/user-create/index.py
+++ lambda_function/user-create/index.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! from github actions')
+        'body': json.dumps('Hello from Lambda! from github actions-first change')
     }
